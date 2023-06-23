bf26420ba2b9f62222b18c1811e87ce105ed2d39
Astrale <ash4567@outlook.fr>
2023-06-23 14:00:48 +0200

    cant run rustpython to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,21 @@
+use std::{rc::Rc, cell::RefCell};
+
+use rustpython::vm::{scope::Scope, builtins::PyDict, PyRef};
+
 wasm_minimal_protocol::initiate_protocol!();
-use pyo3::prelude::*;
+
+static mut buffer : String = String::new();
 
 #[wasm_minimal_protocol::wasm_func]
-pub fn exec(code: String) -> String {
-    pyo3::prepare_freethreaded_python();
-    let mut s = String::default();
-    let x = Python::with_gil(|py| -> PyResult<()> {
-        // let user: String = py.eval(code, None, Some(&locals))?.extract()?;
-        s = py
-            .eval(&code, None, None)
-            .unwrap()
-            .str()
-            .map(|x| x.to_string())
-            .unwrap_or_else(|e| e.to_string());
-        Ok(())
+pub fn exec(code: &str) -> String {
+    let s = code.to_owned();
+    // let s = Rc::new(RefCell::new(vec![]));
+    let res = rustpython::run(move |vm| {
+        let scope = vm.new_scope_with_builtins();
+        let res = vm.run_code_string(scope, &s, "".into());
+        let res = res.unwrap().str(&vm).unwrap().to_string();
+        unsafe{ buffer = res }
     });
-    s
+    // s.borrow_mut().pop().unwrap()
+    unsafe{ buffer.clone()}
 }
